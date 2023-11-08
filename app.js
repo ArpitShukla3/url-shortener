18e0a202d6be705b3153c927cead0fbf45f07291
ArpitShukla3 <118154214+ArpitShukla3@users.noreply.github.com>
2023-11-08 21:25:53 +0530

    6th commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(PORT,()=>{
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors(
-{  origin:'http://localhost:5173',
+{  origin:'https://url-frontend-snowy.vercel.app',
    credentials:true
 }
 ));
